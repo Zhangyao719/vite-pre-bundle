725972435b6a7649604b5aa06ac14484c3f637ab
Zhangyao719 <1258858403@qq.com>
2023-01-30 21:47:19 +0800

    first commit

---
+++ vite-lodash/src/main.js
@@ -0,0 +1,11 @@
+// import { createApp } from 'vue';
+// import './style.css';
+// import App from './App.vue';
+
+// createApp(App).mount('#app');
+
+import uniq from 'lodash-es/uniq.js';
+
+const arr = [1, 2, 3, 3, 4];
+
+console.log(uniq(arr));
